7f8cc5082ed6a55465515d34bd3642ab65b5c596
Simon Inman <simoninman@google.com>
2022-02-24 00:19:54 +0100

    Add absolutely horrible win screen

--- lib/widgets/wordle_grid.dart
+++ lib/widgets/wordle_grid.dart
@@ -11,6 +11,20 @@ class WordleGrid extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final gameState = ref.watch(gameStateProvider);
+    final bool isWinner = gameState.attemptedUpto == 0 
+    ? false 
+    : gameState.attempts[gameState.attemptedUpto - 1] == gameState.correctWord;
+    if (isWinner) {
+      return Stack(children: [
+        _build(context, ref),
+        _winMessage(context, ref)
+      ]);
+    } 
+    return _build(context, ref);
+  }
+
+  Widget _build(BuildContext context, WidgetRef ref) {
+    final gameState = ref.watch(gameStateProvider);
 
     List<Widget> rows = [];
     for (int i = 0; i < gameState.settings.attemptsAllowed; i++) {
@@ -28,5 +42,19 @@ class WordleGrid extends ConsumerWidget {
     return Column(
       children: rows,
     );
+  }
+
+  _winMessage(BuildContext context, WidgetRef ref) { 
+    //todo pass game state
+    final gameState = ref.watch(gameStateProvider);
+    return SizedBox(height: 200.0, width: 400.0, child: Container(color: Colors.blueAccent, 
+    child: Column(children: [
+      const Text('You Won!'),
+      WordleRow(attempted: true, 
+      wordLength: gameState.settings.wordSize, 
+      answer: gameState.correctWord, correct: gameState.correctWord,)
+
+    ],),)
+   ,);
   }
 }
